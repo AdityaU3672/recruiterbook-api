7adf3475de5f331e72a0ddf8143db5420d832770
Rishi Papani <rspapani@outlook.com>
2025-04-09 20:11:06 -0400

    fucking CORS

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://recruiter-rank.com"],
+    allow_origins=["http://localhost:3000", "https://recruiter-rank.com", "https://preview.recruiter-rank.com"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["Content-Type", "Authorization", "Accept", "Origin", "X-Requested-With"],
